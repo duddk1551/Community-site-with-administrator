c77ad8e5e553fc5d2e8880cb1674e228a321b3c1
duddk1551 <duddk1551@gmail.com>
2019-09-23 16:28:22 +0900

    feat:Add post paging and Utils Class

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -1,10 +1,8 @@
 package com.example.demo.service;
 
-import java.util.List;
 import java.util.Map;
 
-import com.example.demo.dto.Article;
 
 public interface ArticleService {
-		public List<Article> getArticleList(Map<String,Object> param);
+		public Map<String,Object> getArticleList(Map<String,Object> param);
 }
